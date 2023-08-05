4fce780b71889971340e26921c192f7d14de56fa
denys_kosyriev <utross2241@gmail.com>
2023-08-05 15:34:31 +0300

    feature: project "portfolio-next-js" and sidebar

---
+++ src/utils/data.js
@@ -0,0 +1,11 @@
+export const dataPortfolio = {
+  photo: '/images/photo.jpg',
+  name: 'Kosyriev Denys',
+  location: 'Kyiv, Ukraine',
+  profession: 'Front-End Developer',
+  skills: [
+    'HTML5/CSS3', 'SCSS/SASS', 'React', 'Next.js', 'BEM', 'JavaScript',
+    'Gulp', 'Git/GitHub', 'Adobe Photoshop', 'Figma', 'Browser Devtools', 'Wordpress',
+    'TypeScript',
+  ]
+}
